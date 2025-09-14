bac47fbab8717166878266d03f64aaee862b22eb
mfernandail <maria.ibanez.leiva@gmail.com>
2025-09-14 20:29:56 -0300

    Add error syntax

--- js/main.js
+++ js/main.js
@@ -10,18 +10,21 @@ const $totalTime = document.getElementById('total-time')
 
 const $settings = document.getElementById('settings')
 
-const $btnWork = document.getElementById('work')
-const $btnShort = document.getElementById('short')
-const $btnLong = document.getElementById('long')
+const $inputWork = document.getElementById('work')
+const $inputShort = document.getElementById('short')
+const $inputLong = document.getElementById('long')
 const $btnSettingClose = document.getElementById('close')
 const $btnSettingSave = document.getElementById('save')
+const $error = document.getElementById('error')
 
 $btnSettings.addEventListener('click', showSetting)
 $btnSettingClose.addEventListener('click', showSetting)
 $btnStart.addEventListener('click', startTimer)
 $btnPause.addEventListener('click', pauseTimer)
+$btnReset.addEventListener('click', resetTimer)
+$btnSettingSave.addEventListener('click', saveSettings)
 
-let currentMinutes = 2
+let currentMinutes = 25
 let currentSeconds = 0
 let intervalId = null
 let isRunning = false
@@ -40,7 +43,12 @@ function startTimer() {
   $btnStart.disabled = true
   $btnPause.disabled = false
 
-  let idInterval = setInterval(() => {
+  currentMinutes = Number($inputWork.value)
+  console.log(typeof currentMinutes)
+
+  if (intervalId) return
+
+  intervalId = setInterval(() => {
     if (currentSeconds > 0) {
       currentSeconds--
     } else {
@@ -48,25 +56,58 @@ function startTimer() {
       currentMinutes--
     }
 
-    $display.textContent = `${String(currentMinutes).padStart(2, '0')}:${String(
-      currentSeconds
-    ).padStart(2, '0')}`
+    renderDisplay(currentMinutes, currentSeconds)
 
     if (currentMinutes === 0 && currentSeconds === 0) {
-      clearInterval(idInterval)
+      clearInterval(intervalId)
       console.log('stop timer')
     }
   }, 1000)
-
-  console.log({ idInterval })
 }
 
 function pauseTimer() {
   $btnStart.disabled = false
   $btnPause.disabled = true
+  clearInterval(intervalId)
+  intervalId = null
+}
+
+function resetTimer() {
+  clearInterval(intervalId)
+  resetControls()
+  if ($inputWork) {
+    currentMinutes = Number($inputWork.value)
+    currentSeconds = 0
+    renderDisplay(currentMinutes, currentSeconds)
+  }
 }
 
 function resetControls() {
   $btnStart.disabled = false
   $btnPause.disabled = true
+  intervalId = null
+}
+
+function renderDisplay(minutes, seconds) {
+  $display.textContent = `${String(minutes).padStart(2, '0')}:${String(
+    seconds
+  ).padStart(2, '0')}`
+}
+
+function saveSettings() {
+  if (!isNaN($inputWork)) {
+    $error.textContent = 'Debe ser un numero'
+    return
+  }
+  if (Number($inputWork.value) > 60) {
+    $error.textContent = 'debe ser menor a 60 minutos'
+    return
+  }
+  $error.textContent = ''
+  $settings.classList.remove('show')
+
+  const minutes = Number($inputWork.value)
+
+  console.log(minutes)
+  renderDisplay(minutes, 0)
 }
