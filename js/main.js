4c9acaa1267697af3340dc00ace3ec3c1c17b52c
mfernandail <maria.ibanez.leiva@gmail.com>
2025-09-13 22:38:44 -0300

    Add btn to main.js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const $btn_start = document.getElementById('start')
+const $btn_pause = document.getElementById('pause')
+const $btn_reset = document.getElementById('reset')
+const $btn_settings = document.getElementById('settings')
