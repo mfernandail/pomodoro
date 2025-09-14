674173df3439a7aef4dcb7487101a10d7162921e
mfernandail <maria.ibanez.leiva@gmail.com>
2025-09-13 22:50:36 -0300

    Add show and hide settings ligical JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-const $btn_start = document.getElementById('start')
-const $btn_pause = document.getElementById('pause')
-const $btn_reset = document.getElementById('reset')
-const $btn_settings = document.getElementById('settings')
+const $display = document.getElementById('display')
+
+const $btnStart = document.getElementById('start')
+const $btnPause = document.getElementById('pause')
+const $btnReset = document.getElementById('reset')
+const $btnSettings = document.getElementById('settings-btn')
+
+const $completed = document.getElementById('completed')
+const $totalTime = document.getElementById('total-time')
+
+const $settings = document.getElementById('settings')
+const $btnSettingClose = document.getElementById('close')
+const $btnSettingSave = document.getElementById('save')
+
+$btnSettings.addEventListener('click', showSetting)
+$btnSettingClose.addEventListener('click', showSetting)
+
+function showSetting() {
+  if ($settings.classList.contains('show')) {
+    $settings.classList.remove('show')
+  } else {
+    $settings.classList.add('show')
+  }
+}
